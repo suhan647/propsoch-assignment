92b087c9e0e4175fe873dd4af49fb8732d4f7fbd
suhan647 <suhanahmed647@gmail.com>
2025-01-18 18:01:30 +0530

    completed

--- propsoch-frontend/components/Map.tsx
+++ propsoch-frontend/components/Map.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
+import { CiLocationOn } from "react-icons/ci";
 
 // Map container style
 const containerStyle = {
@@ -7,8 +8,8 @@ const containerStyle = {
   height: "400px",
 };
 
-// Chikmagalur coordinates
-const chikmagalurCoordinates = {
+// coordinates
+const Coordinates = {
   lat: 13.3185, // Latitude
   lng: 75.7724, // Longitude
 };
@@ -18,15 +19,36 @@ function Map() {
   return (
     <>
      <div style={{ fontSize: '18px', fontWeight:'700', marginTop: '20px', marginBottom:'20px' }}>Location</div>
+     <div style={{ display: 'flex', gap: '20px', alignItems: 'center', marginBottom:'20px' }}>
+      <div style={{ backgroundColor: '#FF750426', borderRadius: '100%', width: '51px', height: '40px', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+        <CiLocationOn color="black" size={16} />
+      </div>
+      <div style={{ fontWeight: 400, fontSize: '12px', color: '#53587A' }}>
+        Jl. Gerungsari, Bulusan, Kec. Tembalang, Kota Semarang, Jawa Tengah 50277
+      </div>
+    </div>
     <LoadScript googleMapsApiKey="AIzaSyBPtfqMSNiaUdz-6x6X45N7wKnCUvv9lAg">
       <GoogleMap
         mapContainerStyle={containerStyle}
-        center={chikmagalurCoordinates}
+        center={Coordinates}
         zoom={12} 
       >
-        <Marker position={chikmagalurCoordinates} />
+        <Marker position={Coordinates} />
       </GoogleMap>
     </LoadScript>
+
+    <div style={{ display: 'flex', justifyContent: 'center', gap: '20px', marginTop: '20px', fontWeight:'600px', fontSize:'10px', color:'#1F4C6B' }}>
+      <div style={{ padding: '10px 20px', backgroundColor: '#f0f0f0', borderRadius: '5px', textAlign: 'center' }}>
+        2 Hospitals
+      </div>
+      <div style={{ padding: '10px 20px', backgroundColor: '#e0f7fa', borderRadius: '5px', textAlign: 'center', }}>
+        4 Gas Stations
+      </div>
+      <div style={{ padding: '10px 20px', backgroundColor: '#f0f0f0', borderRadius: '5px', textAlign: 'center' }}>
+        2 Schools
+      </div>
+    </div>
+
     </>
   );
 }
